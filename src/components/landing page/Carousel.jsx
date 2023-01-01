072856f4412761f9308021e1398dec9e0b0a96b7
ghyawson@gmail.com <Fiifi Yawson>
2023-01-01 21:15:58 +0000

    experiment

--- src/components/landing page/Carousel.jsx
+++ src/components/landing page/Carousel.jsx
@@ -4,29 +4,67 @@ import carouselImage3 from "../../assets/images/carousel images/carousel-image-3
 import carouselImage4 from "../../assets/images/carousel images/carousel-image-4.png"
 import carouselImage5 from "../../assets/images/carousel images/carousel-image-5.png"
 import COCLogo from "../../assets/images/COC KNUST LOGO.png"
-import {useState, useCallback, useEffect} from "react"
+import {useState, useRef, useEffect} from "react"
 import "../../Styles/landing page/Carousel.css"
 
 function Carousel() {  
-  const images = [carouselImage1, carouselImage2, carouselImage3, carouselImage4, carouselImage5]
-  const [currentImage, setCurrentImage] = useState(1)
+  const [images, setImages] = useState({
+    allImages: [carouselImage1, carouselImage2, carouselImage3, carouselImage4, carouselImage5],
+    currentImage: 0
+  })
 
-  const timeout = useCallback(setTimeout(() => {
-    setCurrentImage((state) => (state + 1) % 5)
-  }, 10000), [currentImage])
+  const carousel = useRef()
+  const currentImage = images.allImages[images.currentImage]
+  let currentImageSlide = useRef()
 
+  let interval
+
+  console.log("render", images.allImages.indexOf(images.currentImage))
+
+  const resetCarousel = (scrollAmount) => { 
+    if (interval) clearInterval(interval)
+
+    let n
+    if (scrollAmount < 0) {
+      n = images.allImages.indexOf(currentImage) - 1
+    } else {
+      n = (images.allImages.indexOf(currentImage) + 1) % images.length
+    }
+
+    const arrangedImages = images.allImages
+    if (images.allImages.indexOf(currentImage) === 0) {
+      const image = arrangedImages.pop()
+      arrangedImages.unshift(image)
+
+    } else if (images.allImages.indexOf(currentImage.current) === images.length - 1) {
+      const image = arrangedImages.shift()
+      arrangedImages.push(image)
+    }
+        
+    setImages({
+      ...images,
+      currentImage: n < 0 ? images.length - 1 : n,
+      allImages: arrangedImages
+    })
+
+    // carousel.current.scrollBy(scrollAmount, 0)
+
+    // interval = setInterval(() => {
+    //   resetCarousel(carousel.current.clientWidth)
+    // }, 10000)
+  }
+  
   const carouselNavRight = () => {
-    clearTimeout(timeout)
-    setCurrentImage(state => (state+1)%5)
+    resetCarousel(carousel.current.clientWidth)
   }
 
   const carouselNavLeft = () => {
-    clearTimeout(timeout)
-    setCurrentImage(state => {
-      const n = (state - 1) % 5
-      return n >= 0 ? n :  5 + n
-    })
+    resetCarousel(-carousel.current.clientWidth)
   }
+
+  useEffect(() => {
+    carousel.current.scrollLeft = currentImageSlide.current.offsetLeft
+  })
   
   return (
     <div id="top-section">
@@ -42,8 +80,8 @@ function Carousel() {
         <a href="/" className="nav-option" id="community-nav">Community</a>
       </nav>
       <div id="carousel">
-        <div id="carousel-images-block">
-          {images.map((image, index) => <img key={index} src={image} alt="" className="carousel-image" id={ index === currentImage? "current-carousel-image" : ""} />)}
+        <div id="carousel-images-block" ref={carousel}>
+          {images.allImages.map((image, index) => <img key={index} src={image} alt="" className="carousel-image" id={image === currentImage ? "current-image" : ""} ref={image === currentImage? currentImageSlide: null } />)}
         </div>
         <button id="carousel-nav-right" className="carousel-nav" onClick={carouselNavRight}>&gt;</button>
         <button id="carousel-nav-left" className="carousel-nav" onClick={carouselNavLeft}>&lt;</button>
